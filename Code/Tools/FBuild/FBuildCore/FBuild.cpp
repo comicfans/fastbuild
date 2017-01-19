414c25d796dcdc090acf0fc2a1edfebefdaaf669
comicfans44 <comicfans44@gmail.com>
2017-01-19 23:18:16 +0800

    makes MSVC (incorrect) warning quiet

--- Code/Tools/FBuild/FBuildCore/FBuild.cpp
+++ Code/Tools/FBuild/FBuildCore/FBuild.cpp
@@ -305,6 +305,10 @@ struct SaveTrigger
 {
     FBuild *instance;
 
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable: 6011)
+#endif
     ~SaveTrigger()
     {
         ASSERT(instance);
@@ -314,6 +318,9 @@ struct SaveTrigger
             instance->SaveDependencyGraph( instance->m_DependencyGraphFile.Get() );
         }
     }
+#ifdef _MSVC_VER
+#pragma warning( pop )
+#endif
 };
 
 // Build
